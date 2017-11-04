1ac86ef66fbac6f367b13882db53f33e3bbae070
jitniran <jitshakes@gmail.com>
2017-11-04 22:08:17 +0530

    update query.py

--- query.py
+++ query.py
@@ -4,18 +4,20 @@ import json
 import psycopg2
 from tabulate import tabulate
 
-def run_queries(query):
-    """connects with database runs the query and returns result
-    query= sql query"""
 
-    #connection string
+def run_queries(query):
+    """
+    connects with database runs the query and returns result
+    query= sql query
+    """
+    # connection string
     conn_string = "dbname=news"
     print("Connecting to database ->{}\n".format(conn_string))
 
-    #connect to database
+    # connect to database
     conn = psycopg2.connect(conn_string)
 
-    #return a cursor
+    # return a cursor
     cursor = conn.cursor()
 
     # execute the queries and fetch reults
@@ -23,15 +25,18 @@ def run_queries(query):
     cursor.execute(query)
     results = cursor.fetchall()
 
-    #close connection
+    # close connection
     conn.close()
     return results
 
+
 def write_file(header, title, results):
-    """writes to file called log.txt
+    """
+    writes to file called log.txt
     header = result header
     title = Question
-    results = query results"""
+    results = query results
+    """
 
     table = []
     for result in results:
@@ -41,19 +46,20 @@ def write_file(header, title, results):
         table.append(item)
 
     with open('log.txt', 'a') as log_file:
-        log_file.write("%s\n\n" %title)
+        log_file.write("%s\n\n" % title)
         log_file.write(tabulate(table, header, tablefmt='presto'))
         log_file.write('\n\n')
 
+
 def main():
     """ reads json file for data"""
-    #read the file contents
+    # read the file contents
     with open('data.json') as json_file:
         file_data = json.load(json_file)
 
     data = file_data['data']
 
-    #views = file_data['views']
+    # views = file_data['views']
 
     # results = runQueries(data[0]['query'])
     # writeToFile(data[0]['header'],data[0]['question'],results)
@@ -65,4 +71,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
